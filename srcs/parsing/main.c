45e2a0d4d785593a62869c278520aae8d11b5720
Rasaboun <rasaboun@student.42.fr>
2021-08-21 17:13:53 +0200

    CLEAN

--- srcs/parsing/main.c
+++ srcs/parsing/main.c
@@ -1,52 +1,10 @@
 
 #include"minishell.h"
 
-
 void sig_handler(int signum)
 {
   exit(0);
 }
-//
-void	ft_cutcmd(t_cmd **cmd, char *line)
-{
-	int	i;
-	char *l;
-	int n;
-	t_cmd *tmp;
-	char **split;
-
-	split = NULL;
-	n = 0;
-	l = NULL;
-	i = 0;
-
-	while (line[i] != '\0')
-	{
-		if (line[i] == '|' || line[i] == ';')
-		{
-			l = ft_substr(line,n,i-n);
-			split = ft_split(l,' ');
-			free(l);
-			tmp = ft_tcmdnew(split);
-			if (line[i] == '|')
-				tmp->type = PIPED;
-			if (line[i] == ';')
-				tmp->type = BREAK;
-			ft_tcmdadd_back(cmd,tmp);
-			i++;
-			n = i;
-		}
-		else
-			i++;
-	}
-		l = ft_substr(line,n,i-n);
-		split = ft_split(l,' ');
-		free(l);
-		tmp = ft_tcmdnew(split);
-		tmp->type = END;
-		ft_tcmdadd_back(cmd,tmp);
-}
-
 
 int main()
 {
