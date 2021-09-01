c692b21bcb2ec20617b0be40f49286d29fded32d
Koriakin <danielkor94@gmail.com>
2021-09-01 10:05:18 +0200

    echo 2

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: dkoriaki <dkoriaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 11:19:07 by dkoriaki          #+#    #+#             */
-/*   Updated: 2021/08/25 22:11:10 by dkoriaki         ###   ########.fr       */
+/*   Updated: 2021/08/25 22:16:31 by dkoriaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	ft_echo(t_cmd *ccmd)
 
 	new_line = 0;
 	i = 1;
+	printf("carrote\n");
 	if (ccmd->args[1] && ft_strcmp(ccmd->args[1], "-n") == 0)
 		new_line = 1;
 	while (ccmd->args[i])	
