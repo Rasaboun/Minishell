f967f83576b5ab11fa456cf74de4545a35fa2b2a
Rasaboun <rasaboun@student.42.fr>
2021-10-02 14:31:33 +0200

    Daniel2

--- srcs/parsing/main.c
+++ srcs/parsing/main.c
@@ -6,13 +6,11 @@
 /*   By: rasaboun <rasaboun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 11:18:37 by dkoriaki          #+#    #+#             */
-/*   Updated: 2021/10/02 14:28:04 by rasaboun         ###   ########.fr       */
+/*   Updated: 2021/10/02 14:31:07 by rasaboun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-
 
 t_cmd *ccmd;
 
@@ -40,32 +38,28 @@ void	ft_freecmd(t_cmd *cmd)
 
 void sig_handler(int signum)
 {
+  ft_freecmd(ccmd);
   exit(0);
 }
 
 void	ft_init_minishell(t_minishell *minishell, char **envp)
 {
-	while(g_ccmd != NULL) // AFFICHE CHAQUE COMMANDE AVEC SON TYPE
-	{
-		printf("%s",g_ccmd->args[0]);
-		if (g_ccmd->type == PIPED)
-			printf(" TYPE PIPED\n");
-		if (g_ccmd->type == BREAK)
-			printf(" TYPE BREAK\n");
-		if (g_ccmd->type == END)
-			printf(" TYPE END\n");
-		g_ccmd = g_ccmd->next;
-	}
+	minishell->env = ft_init_env(envp);
+	minishell->exit = 0;
+	minishell->ret = 0;
+	minishell->stdout = dup(STDOUT_FILENO);
+	minishell->stdin = dup(STDIN_FILENO);
 }
 
 int main(int ac, char **av, char **envp)
 {
 	(void)ac;
 	(void)av;
-	char *cmd;
-	t_env *env;
-	t_cmd *tmp;
-	g_ccmd = NULL;
+	char			*cmd;
+	//t_env			*env;
+	t_minishell		minishell;
+
+	ccmd = NULL;
 	cmd = NULL;
 	//env = ft_init_env(envp);
 	ft_init_minishell(&minishell, envp);
@@ -77,17 +71,15 @@ int main(int ac, char **av, char **envp)
 		cmd = readline("\x1b[36m❯ \x1b[35m(Minishell)\x1b[37m ");
 		if (cmd != NULL)
 		{
-			ft_cutcmd(&g_ccmd,cmd);
-			/*while (g_ccmd)
-			{
-				for (int y = 0; g_ccmd->args[y];y++)
-					printf("|%s|",g_ccmd->args[y]);
-				g_ccmd = g_ccmd->next;
-			}*/
-			
-			exit(0);
-			//exec_cmds(ccmd, env);
-			//break;
+			add_history(cmd);
+			ft_cutcmd(&ccmd,cmd);
+			minishell.ret = exec_cmds(ccmd, &minishell);
+			//printcmd();
+			free(cmd);
+			ft_freecmd(ccmd);
+			ccmd = NULL;
+			//if (mini->exit == 1)
+				//break;
 		}
 		
 	}
