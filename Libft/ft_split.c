d4dc43b2b1acc4628de6f84a09d92d0b0ca4955f
Koriakin <danielkor94@gmail.com>
2021-10-06 12:05:46 +0200

    try with other ft_split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -3,87 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rasaboun <rasaboun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dkoriaki <dkoriaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 17:51:59 by rasaboun          #+#    #+#             */
-/*   Updated: 2019/11/07 14:30:49 by rasaboun         ###   ########.fr       */
+/*   Updated: 2021/10/06 12:05:09 by dkoriaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_substrr(const char *s, int min, int max)
+static void		*ft_strfree(char **str)
 {
-	int		i;
-	char	*s2;
+	int	i;
 
-	if (!(s2 = malloc(sizeof(char*) * (max - min + 1))))
-		return (NULL);
 	i = 0;
-	while (min < max)
-	{
-		s2[i] = s[min];
+	while (str[i])
 		i++;
-		min++;
-	}
-	s2[i] = '\0';
-	return (s2);
-}
-
-static int	ft_count(const char *s, char c)
-{
-	int i;
-	int n;
-
-	n = 0;
-	i = 0;
-	if (s[i] == '\0')
-		return (n);
-	if (s[0] != c)
-		n++;
-	while (s[i + 1] != '\0')
+	while (i > 0)
 	{
-		if (s[i] == c && s[i + 1] != c)
-			n++;
-		i++;
+		i--;
+		free(str[i]);
 	}
-	return (n);
+	free(str);
+	return ((void *)0);
 }
 
-static void	ft_free(int n, char **s)
+static int		ft_nbwrds(char const *s, char c)
 {
-	while (n >= 0)
-	{
-		free(s[n]);
-		n--;
-	}
-}
-
-char		**ft_split(char const *s, char c)
-{
-	int		i;
-	int		min;
-	char	**s2;
-	int		n;
+	int	i;
+	int	j;
 
 	if (!s)
-		return (NULL);
-	n = 0;
-	if (!(s2 = malloc(sizeof(char*) * (ft_count(s, c) + 1))))
-		return (0);
-	s2[ft_count(s, c)] = 0;
+		return (-1);
 	i = 0;
-	while (s[i] != '\0')
+	j = 0;
+	while (s[i])
 	{
-		while (s[i] && s[i] == c)
+		if (s[i] != c)
+		{
+			j++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+		else
 			i++;
-		min = i;
-		while (s[i] && s[i] != c)
-			i++;
-		if (n < ft_count(s, c))
-			if (!(s2[n] = ft_substrr(s, min, i)))
-				ft_free(n, s2);
-		n++;
 	}
-	return (s2);
+	return (j);
+}
+
+static int		w(char const *s, int i, char c)
+{
+	while (s[i] != c && s[i])
+		i++;
+	return (i + 1);
+}
+
+char			**ft_split(char const *str, char c)
+{
+	char	**out;
+	int		i[3];
+
+	if ((ft_nbwrds(str, c) == -1)
+		|| !(out = (char **)malloc(sizeof(char *) * (ft_nbwrds(str, c) + 1))))
+		return (NULL);
+	i[0] = 0;
+	i[1] = 0;
+	while (str[i[0]])
+	{
+		if (str[i[0]] != c)
+		{
+			if (!(out[i[1]] = (char *)malloc(sizeof(char) * w(str, i[0], c))))
+				return (ft_strfree(out));
+			i[2] = 0;
+			while (str[i[0]] != c && str[i[0]])
+				out[i[1]][i[2]++] = str[i[0]++];
+			out[i[1]][i[2]] = '\0';
+			i[1]++;
+		}
+		else
+			i[0]++;
+	}
+	out[i[1]] = NULL;
+	return (out);
 }
