49042d03d08ab388400a53b7fa2d92bfad0474b2
user42 <dkoriaki@student.42.fr>
2021-10-10 13:28:14 +0200

    c'est bientot la fin

--- srcs/execution/redirection.c
+++ srcs/execution/redirection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rasaboun <rasaboun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dkoriaki <dkoriaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 10:57:59 by dkoriaki          #+#    #+#             */
-/*   Updated: 2021/10/10 01:24:44 by rasaboun         ###   ########.fr       */
+/*   Updated: 2021/10/10 12:54:31 by dkoriaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_redir_output_trunc(char **args, int i)
 		ft_write_error("minishell: ");
 		ft_write_error(args[i + 1]);
 		ft_write_error(": No such file or directory\n");
-		return (FAILURE);
+		return (2);
 	}
 	dup2(fd_out, STDOUT_FILENO);
 	ft_close(fd_out);
@@ -38,6 +38,11 @@ int	ft_redir_output_append(char **args, int i)
 {
 	int		fd_out;
 
+	if (args[i + 1][0] == '\0')
+	{
+		ft_write_error("minishell: ambiguous redirect\n");
+		return (2);
+	}
 	fd_out = open(args[i + 1], O_WRONLY
 			| O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
 	if (fd_out == -1)
@@ -45,7 +50,7 @@ int	ft_redir_output_append(char **args, int i)
 		ft_write_error("minishell: ");
 		ft_write_error(args[i + 1]);
 		ft_write_error(": No such file or directory\n");
-		return (FAILURE);
+		return (2);
 	}
 	dup2(fd_out, STDOUT_FILENO);
 	ft_close(fd_out);
@@ -56,6 +61,11 @@ int	ft_redir_input(char **args, int i)
 {
 	int		fd_in;
 
+	if (args[i + 1][0] == '\0')
+	{
+		ft_write_error("minishell: ambiguous redirect\n");
+		return (2);
+	}
 	fd_in = open(args[i + 1], O_RDONLY, S_IRUSR);
 	if (fd_in == -1)
 	{
